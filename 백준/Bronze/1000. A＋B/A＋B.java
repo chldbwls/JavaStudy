82fa9de4379504c58967291dd96908bd5353936f
chldbwls <82202665+chldbwls@users.noreply.github.com>
2024-03-08 12:42:02 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
-	int a = 0;
-	int b = 0;
-
-	a = sc.nextInt();
-	b = sc.nextInt();
-	int c = 0;
-	c = a + b;
-	System.out.print(c);
-	sc.close();
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.print(a+b);
+        
+        scanner.close();
 	}
 }
