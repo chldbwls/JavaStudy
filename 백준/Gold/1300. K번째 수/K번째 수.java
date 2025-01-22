de26de507e3fdfdea6da69c6608e0b75bc8867b5
chldbwls <82202665+chldbwls@users.noreply.github.com>
2025-01-22 13:10:17 +0900

    [Gold I] Title: K번째 수, Time: 168 ms, Memory: 15928 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        long start = 1; 
+        long end = (long) N * N;
+        long result = 0;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long count = 0;
+
+            for (int i = 1; i <= N; i++) {
+                count += Math.min(mid / i, N);
+            }
+
+            if (count >= K) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
