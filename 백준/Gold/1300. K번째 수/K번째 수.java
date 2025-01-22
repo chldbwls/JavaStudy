3c863e9a9f370c195123a8a46e74553d2e724360
chldbwls <82202665+chldbwls@users.noreply.github.com>
2025-01-22 13:10:47 +0900

    [Gold I] Title: K번째 수, Time: 160 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,4 +1,6 @@
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
@@ -7,28 +9,27 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
-
-        long start = 1; 
-        long end = (long) N * N;
+        long start = 1;
+        long end = (long)N*N;
         long result = 0;
 
-        while (start <= end) {
-            long mid = (start + end) / 2;
+        while(start<=end){
+            long mid = (start+end)/2;
             long count = 0;
-
-            for (int i = 1; i <= N; i++) {
-                count += Math.min(mid / i, N);
+            for(int i=1;i<=N;i++){
+                count+=Math.min(mid/i,N);
             }
 
-            if (count >= K) {
+            if(count>=K){
                 result = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+                end = mid-1;
+            }else{
+                start=mid+1;
             }
         }
 
-        bw.write(result + "\n");
+        bw.write(result+"\n");
+
         bw.flush();
         bw.close();
         br.close();
