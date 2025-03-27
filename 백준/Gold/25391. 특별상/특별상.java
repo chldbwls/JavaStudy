5265e92e094256e1c363f1844fd8cfbaa052765f
chldbwls <82202665+chldbwls@users.noreply.github.com>
2025-03-27 14:26:18 +0900

    [Gold V] Title: 특별상, Time: 1548 ms, Memory: 86268 KB -BaekjoonHub

---
+++ 백준/Gold/25391. 특별상/특별상.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] == b[1]) return b[0] - a[0];
+            else return b[1] - a[1];
+        });
+
+        long max = 0;
+        for (int i = 0; i < K; i++) {
+            max += arr[i][0];
+        }
+
+        int[][] arr2 = new int[N - K][2];
+        for (int i = K; i < N; i++) {
+            arr2[i - K][0] = arr[i][0];
+            arr2[i - K][1] = arr[i][1];
+        }
+
+        Arrays.sort(arr2, (a, b) -> {
+            if (a[0] == b[0]) return b[1] - a[1];
+            else return b[0] - a[0];
+        });
+
+        for (int i = 0; i < M; i++) {
+            max += arr2[i][0];
+        }
+
+        bw.write(max + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
